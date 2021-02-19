5e9324e5256b3cd27659b5c4ed6d779d697e8245
faliks <243650719@qq.com>
2021-02-19 17:49:00 +0800

    model add

---
+++ VGGNet/vggnet.py
@@ -0,0 +1,159 @@
+# import the necessary packages
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+
+class SmallVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # CONV => RELU => POOL
+        model.add(Conv2D(32, (3, 3), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
+
+
+class VGG16:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # block 1
+        model.add(Conv2D(64, (3, 3), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding="same", ))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # block 2
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # block 3
+        model.add(Conv2D(256, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(256, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(256, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # block 4
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # block 5
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(512, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(4096))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(4096))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        return model
--- train.py
+++ train.py
@@ -1,19 +1,38 @@
+import matplotlib
+
+matplotlib.use("Agg")
 import argparse
 import cv2
-from common import classes
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import img_to_array
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+from VGGNet.vggnet import SmallVGGNet
+from VGGNet.vggnet import VGG16
 from utils import get_class
 from imutils import paths
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
                 help="path to input dataset of images")
-# ap.add_argument("-m", "--model", required=True,
-#                 help="path to output trained model")
-# ap.add_argument("-l", "--label-bin", required=True,
-#                 help="path to output label binarizer")
-# ap.add_argument("-p", "--plot", required=True,
-#                 help="path to output accuracy/loss plot")
+ap.add_argument("-m", "--model", required=True,
+                help="path to output trained model")
+ap.add_argument("-l", "--label-bin", required=True,
+                help="path to output label binarizer")
+ap.add_argument("-p", "--plot", required=True,
+                help="path to output accuracy/loss plot")
 args = vars(ap.parse_args())
+
+INIT_LR = 0.001
+EPOCHS = 75
+BS = 1
+
+print("[INFO] read image...")
 
 imagePaths = sorted(list(paths.list_images(args["dataset"])))
 
@@ -24,9 +43,62 @@ for imagePath in imagePaths:
     label = get_class.get_class(imagePath)
     if label == "null":
         continue
-    print(label)
+    # print(label)
     image = cv2.imread(imagePath)
-    image = image.resize(64, 64)
+    image = cv2.resize(image, (224, 224))
+    image = img_to_array(image)
+    # image = image / 255.0
+    # image = np.expand_dims(image, 0)
 
     data.append(image)
     labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+(trainX, testX, trainY, testY) = \
+    train_test_split(data, labels, test_size=0.2, random_state=42)
+
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
+
+print("[INFO] compiling model...")
+model = VGG16.build(width=64, height=64, depth=3, classes=len(lb.classes_))
+opt = SGD(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+print("[INFO] training network...")
+H = model.fit(x=aug.flow(trainX, trainY, batch_size=BS),
+              validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+              epochs=EPOCHS)
+
+print("[INFO] evaluating network...")
+predictions = model.predict(x=testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+                            predictions.argmax(axis=1), target_names=lb.classes_))
+
+# plot the training loss and accuracy
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["accuracy"], label="train_acc")
+plt.plot(N, H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy (SmallVGGNet)")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["plot"])
+
+# save the model and label binarizer to disk
+print("[INFO] serializing network and label binarizer...")
+model.save(args["model"], save_format="h5")
+f = open(args["label_bin"], "wb")
+f.write(pickle.dumps(lb))
+f.close()
